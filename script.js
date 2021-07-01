12ad764eb53efb397e074b8bf2182aff154f121e
wilsonkinyua <wilsonkinyuam@gmail.com>
2021-07-01 22:25:03 +0300

    getting input value

--- script.js
+++ script.js
@@ -11,6 +11,29 @@ document.querySelector(".guess").value = 24;
 console.log(document.querySelector(".guess").value);
 */
 
+const secretNumber = Math.trunc(Math.random() * 20 + 1);
+
+let score = document.querySelector(".score").textContent;
+
+document.querySelector(".number").textContent = secretNumber;
+
+console.log(secretNumber);
+
 document.querySelector('.check').addEventListener('click', function() {
-    console.log(document.querySelector(".guess").value);
+    const guess = Number(document.querySelector(".guess").value);
+    console.log(guess, typeof guess);
+    if(!guess) {
+        document.querySelector(".message").textContent = "No number ❌";
+    } else if(guess === secretNumber) {
+        document.querySelector(".message").textContent = "🎉 Correct Number!";
+        // document.querySelector(".guess").value = '';
+    } else if(guess > secretNumber) {
+        document.querySelector(".message").textContent = "🙂 Number too High!";
+        score--;
+        document.querySelector(".score").textContent = score;
+    } else if(guess < secretNumber) {
+        document.querySelector(".message").textContent = "😞 Number too low!";
+        score--;
+        document.querySelector(".score").textContent = score;
+    }
 });
